7da967ed5e1911b235892299e01611c5d07ad1b2
aquanovae <ericravaz@protonmail.com>
2025-04-07 18:54:23 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ mod spotify;
 
 use anyhow::Result;
 use clap::{ Parser, Subcommand };
-use tokio::runtime::Runtime;
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -16,7 +15,13 @@ enum Command {
     Auth {
         #[arg(short, default_value_t = false)]
         server_login: bool,
-    }
+    },
+    /// Generate daily playlist
+    Generate,
+    /// Switch current playing track to selected playlist
+    SwitchTrack,
+    /// Get current playing track info
+    TrackInfo,
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -27,13 +32,28 @@ enum Error {
     ParseAuthResponse,
 }
 
-fn main() -> Result<()> {
-    let rt = Runtime::new()?;
-    rt.block_on(async {
-        let cli = Cli::parse();
-        match cli.command {
-            Command::Auth{ server_login } => spotify::authenticate(server_login).await?,
-        };
-        Ok(())
-    })
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cli = Cli::parse();
+    match cli.command {
+        Command::Auth{ server_login } => {
+            spotify::authenticate(server_login).await?;
+        },
+        _ => run_with_api(cli.command).await?,
+    };
+    Ok(())
+}
+
+async fn run_with_api(command: Command) -> Result<()> {
+    let spotify = spotify::get_api().await?;
+    match command {
+        Command::Generate => {
+        },
+        Command::SwitchTrack => {
+        },
+        Command::TrackInfo => {
+        },
+        _ => (),
+    };
+    Ok(())
 }
--- src/spotify.rs
+++ src/spotify.rs
@@ -13,23 +13,28 @@ use std::{
     net::TcpListener,
     path::Path,
     process::Command,
-    time::Duration,
 };
 use url::Url;
 
+pub type Spotify = Client<Token, AuthCodePkceFlow, NoVerifier>;
+
 const CLIENT_ID: &str = "b91f8140e4014e0eaf126d0bb043f59c";
 const LOOPBACK_ADDRESS: &str = "127.0.0.1:8080";
-const CACHE_FILE: &str = "/var/cache/spotify-token/token.txt";
+//const CACHE_FILE: &str = "/var/cache/spotify-token/token.txt";
+const CACHE_FILE: &str = "/tmp/spotify-token/token.txt";
+
+pub async fn get_api() -> Result<Spotify> {
+    let mut cache_file = File::open(CACHE_FILE)?;
+    let mut token = String::new();
+    cache_file.read_to_string(&mut token)?;
+    let auth = AuthCodePkceFlow::new(CLIENT_ID, scopes());
+    let spotify = Client::from_refresh_token(auth, true, token).await?;
+    Ok(spotify)
+}
 
 pub async fn authenticate(server_mode: bool) -> Result<()> {
     let url = RedirectUrl::new(String::from("http://") + LOOPBACK_ADDRESS)?;
-    let scopes = vec![
-        "playlist-read-private",
-        "playlist-modify-private",
-        "user-library-modify",
-        "user-read-currently-playing",
-    ];
-    let auth = AuthCodePkceFlow::new(CLIENT_ID, scopes);
+    let auth = AuthCodePkceFlow::new(CLIENT_ID, scopes());
     let (client, url) = AuthCodePkceClient::new(auth, url, true);
     let (code, state) = if server_mode {
         prompt_cli(url.as_str())?
@@ -40,8 +45,18 @@ pub async fn authenticate(server_mode: bool) -> Result<()> {
     let refresh_token = spotify.refresh_token().ok_or(Error::NoRefreshToken)?;
     let cache_path = Path::new(CACHE_FILE).parent().unwrap();
     fs::create_dir_all(cache_path)?;
-    let cache_file = File::create(CACHE_FILE)?;
+    let mut cache_file = File::create(CACHE_FILE)?;
+    cache_file.write_all(refresh_token.as_bytes())?;
     Ok(())
+}
+
+fn scopes() -> Vec<&'static str> {
+    vec![
+        "playlist-read-private",
+        "playlist-modify-private",
+        "user-library-modify",
+        "user-read-currently-playing",
+    ]
 }
 
 fn prompt_browser(url: &str) -> Result<(String, String)> {
